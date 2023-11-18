708269d9380cf69a1685cb4f7ac2ceb0aa4ecf12
MOHAMED IKEN <103695603+mohamediken@users.noreply.github.com>
2023-11-18 15:03:53 +0100

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,6 +7,7 @@ import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 import { TypeAnimation } from "react-type-animation";
 import resumePDF from "../../assets/Resume.pdf";
+import myImg from "../../assets/DSC_0062.png";
 
 
 const Hero = () => {
@@ -92,7 +93,7 @@ const Hero = () => {
             <img
             data-aos="fade-up"
               className=" h-[90%]  w-full object-cover md:h-[95%] md:m-auto sm:m-0"
-              src='\src\assets\DSC_0062.png'
+              src={myImg}
               // src={mine}
               alt="mine"
             />
