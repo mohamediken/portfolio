cdcb120c0afb9e7446e7e5bee1869dc0869bb40c
MOHAMED IKEN <103695603+mohamediken@users.noreply.github.com>
2023-11-18 15:16:05 +0100

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -93,7 +93,7 @@ const Hero = () => {
             <img
             data-aos="fade-up"
               className=" h-[90%]  w-full object-cover md:h-[95%] md:m-auto sm:m-0"
-              href={myImg}
+              src={myImg}
               // src={mine}
               alt="mon image"
             />
